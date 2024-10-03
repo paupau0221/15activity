597e06c36d9a265872d6b23842e88ed8db52141e
paupau0221 <paulinesoler2929@gmail.com>
2024-10-04 03:50:51 +0400

    software

--- src/app/components/software/software.component.ts
+++ src/app/components/software/software.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrl: './software.component.css'
 })
 export class SoftwareComponent {
+  softwar: string[] = [ "software1"];
+  item: string = '';
 
+  addItems() {
+    this.softwar.push(this.item);
+  }
+
+  deleteGame(index: number) {
+    this.softwar.splice(index, 1);
+  }
 }
