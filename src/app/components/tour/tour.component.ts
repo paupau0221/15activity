023c98b0057cbd5abe0dcde211409243958a9e8e
paupau0221 <paulinesoler2929@gmail.com>
2024-10-04 03:56:07 +0400

    tour

--- src/app/components/tour/tour.component.ts
+++ src/app/components/tour/tour.component.ts
@@ -10,8 +10,7 @@ interface TourDate {
   styleUrl: './tour.component.css'
 })
 export class TourComponent {
-  tourDates: TourDate[] = [
-    { city: "New York", date: new Date('2024-03-15') },
+  tourDate: TourDate[] = [
     { city: "Los Angeles", date: new Date('2024-03-22') },
     { city: "Chicago", date: new Date('2024-03-29') }
   ];
@@ -20,12 +19,12 @@ export class TourComponent {
 
   addTourDate() {
     const newTourDate: TourDate = { city: this.newCity, date: this.newDate };
-    this.tourDates.push(newTourDate);
+    this.tourDate.push(newTourDate);
     this.newCity = '';
     this.newDate = new Date();
   }
 
   deleteTourDate(index: number) {
-    this.tourDates.splice(index, 1);
+    this.tourDate.splice(index, 1);
   }
 }
