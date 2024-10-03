45ddb419b1d12b98502aaca50aaef9a8df40b3b6
paupau0221 <paulinesoler2929@gmail.com>
2024-10-04 03:23:35 +0400

    inventory

--- src/app/components/inventory/inventory.component.ts
+++ src/app/components/inventory/inventory.component.ts
@@ -9,22 +9,22 @@ interface InventoryItem {
   styleUrl: './inventory.component.css'
 })
 export class InventoryComponent {
-  inventory: InventoryItem[] = [
-    { name: "T-Shirt", quantity: 20 },
-    { name: "Jeans", quantity: 15 },
-    { name: "Shoes", quantity: 10 }
+  inventor: InventoryItem[] = [
+    { name: "Shoes", quantity: 25 },
+    { name: "T-Shirt", quantity: 15 },
+    { name: "Pants", quantity: 12 }
   ];
   newItem: string = '';
   newQuantity: number = 0;
 
   addItem() {
     const newItem: InventoryItem = { name: this.newItem, quantity: this.newQuantity };
-    this.inventory.push(newItem);
+    this.inventor.push(newItem);
     this.newItem = '';
     this.newQuantity = 0;
   }
 
   deleteItem(index: number) {
-    this.inventory.splice(index, 1);
+    this.inventor.splice(index, 1);
   }
 }
