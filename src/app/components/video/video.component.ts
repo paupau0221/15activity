3e28222061617270f84eed36ba7439d7c35c678c
paupau0221 <paulinesoler2929@gmail.com>
2024-10-04 04:01:13 +0400

    video

--- src/app/components/video/video.component.ts
+++ src/app/components/video/video.component.ts
@@ -8,20 +8,20 @@ interface Video {
   styleUrl: './video.component.css'
 })
 export class VideoComponent {
-  videoList: Video[] = [
-    { title: "The Shawshank Redemption" },
-    { title: "The Dark Knight" },
-    { title: "Pulp Fiction" }
+  videoLis: Video[] = [
+    { title: "Unbelievable Transformation: How I Changed My Life in 30 Days!" },
+    { title: "Top 10 Mind-Blowing Facts You Didn't Know About Space!" },
+    { title: "Epic Cooking Challenge: Can We Make a Gourmet Meal for Under $10?" }
   ];
   newVideoTitle: string = '';
 
   addVideo() {
     const newVideo: Video = { title: this.newVideoTitle };
-    this.videoList.push(newVideo);
+    this.videoLis.push(newVideo);
     this.newVideoTitle = '';
   }
 
   deleteVideo(index: number) {
-    this.videoList.splice(index, 1);
+    this.videoLis.splice(index, 1);
   }
 }
